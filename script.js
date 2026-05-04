1a59f9d313cc73b70e9d4685dc11d79fec5de10d
Aryan <pounikararyan@gmail.com>
2026-05-04 19:53:20 +0530

    fix: bug and classes

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let menuBtn = document.querySelector("#menuIcon i");
 let navElement = document.querySelector("nav");
+let navListElement = document.querySelectorAll("nav ul li a");
+
 menuBtn.addEventListener("click" , () => {
     menuBtn.classList.toggle("fa-xmark");
     menuBtn.classList.toggle("fa-bars");
     navElement.classList.toggle("left-0");
 })
+
+navListElement.forEach((list, index) => {
+    list.addEventListener("click" , () => {
+    navElement.classList.toggle("left-0");
+})
+})
+
